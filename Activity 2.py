07bee7d048df57683b38c1e267d0771da0578a21
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-09-04 13:50:16 -0500

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,10 @@
+# This program demonstrates arithmetic operations.
+x = 10
+y = 27
+print("x=" + str(x))
+print("y=" + str(y))
+print("x+y=" + str(x + y))
+print("x-y=" + str(x - y))
+print("x*y=" + str(x * y))
+print("x/y=" + str(float(x) / y))
+print("x%y=" + str(x % y))

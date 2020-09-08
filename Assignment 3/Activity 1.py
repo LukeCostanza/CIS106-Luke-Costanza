235cd5f2bd34a5ecb3e9683a08d1e7b3fb5fe3c6
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-09-07 22:17:07 -0500

    Delete Activity 1.py

--- Assignment 3/Activity 1.py
+++
@@ -1,9 +0,0 @@
-# This program demonstrates variables, literal constants, and data types.
-a = 1234567890
-b = 1.23456789012345
-c = "string"
-d = True
-print("integer a=" + str(a))
-print("real b=" + str(b))
-print("string c=" + c)
-print("boolean d=" + str(d))

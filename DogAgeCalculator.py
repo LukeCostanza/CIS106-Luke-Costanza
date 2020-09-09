ca3d1b004e1f98bfa81039c27279d62f95afac9c
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-09-08 23:16:19 -0500

    Add files via upload

---
+++ DogAgeCalculator.py
@@ -0,0 +1,5 @@
+#This program Displays the age of a dog in dog years in relation to human years
+name=str(input("your dogs name: "))
+age=int(input("your dogs age in human years: "))
+Age=(int(age*7))
+print(name +"'s age in dog years is: " + str(Age))

023397dfc8a383586d72b28efab97d42c4aa8a6f
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-09-16 00:05:50 -0500

    Add files via upload

---
+++ Activity 6.py
@@ -0,0 +1,25 @@
+#This program will determin the cost and amount of paint needed for a room (6)
+
+length = int(input("length of room in Ft: "))
+
+width = int(input("width of room in Ft: "))
+
+height = int(input("height of room in Ft: "))
+
+TotalArea = (2 * length * height + 2 * width * height)
+
+price = float(input("price for a gallon of paint to the .00 decimal point (Ex: 10.24) "))
+
+square = int(input("amount of square feet your gallon of paint will cover: "))
+
+gallon = TotalArea / square
+
+RoundedGallon = (.9999 + gallon)
+
+cost = price * RoundedGallon
+
+print(str("size of your room in square Ft is: ") + str (TotalArea)+ str(" Square Ft"))
+
+print(gallon)
+
+print(cost)

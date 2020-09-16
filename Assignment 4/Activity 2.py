0109970053e5163a2e0ce35774e2b84e54314d96
Dave Braunschweig <dbraunsc@harpercollege.edu>
2020-09-16 10:07:10 -0500

    Assignment 4 corrections

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -1,16 +1,13 @@
-#this program will determan the users age in months days hours and seconds
+# This program will determan the users age in months days hours and seconds.
+
 age = int(input("How old are you? "))
 
-month = age*12
+month = age * 12
+day = age * 365
+hour = day * 24
+seconds = hour * 60 * 60
 
-day = month*365
-
-hour = day*24
-
-seconds = hour*60
-
-print ("your age in months is: " + str(month))
-
-print ("your age in days is: " + str(day))
-print ("your age in hours is: " + str(hour))
-print ("your age in seconds is: " + str(seconds))
+print("your age in months is: " + str(month))
+print("your age in days is: " + str(day))
+print("your age in hours is: " + str(hour))
+print("your age in seconds is: " + str(seconds))

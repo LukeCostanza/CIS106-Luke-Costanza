42a7ffd7f4cfe01c0f9895c4e94d2ee92cd1d757
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-09-16 18:19:06 -0500

    Update Activity 2.py

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -5,7 +5,7 @@ age = int(input("How old are you? "))
 month = age * 12
 day = age * 365
 hour = day * 24
-seconds = hour * 60 * 60
+seconds = hour * 60 * 60 #could also do (hour * 3600) correct?
 
 print("your age in months is: " + str(month))
 print("your age in days is: " + str(day))

b651c9968a5e5590d66b7ad46a63b4908f535539
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-09-21 22:16:58 -0500

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,24 @@
+  # This program will determan the users age in months days hours and seconds.
+
+def Processinput (age) : 
+  pass
+def Processmonth (month) :
+  pass
+def Processday (day) :
+  pass
+def Processhour (hour) :
+  pass
+def Processseconds (seconds) :
+  pass
+
+age = int(input("How old are you? "))
+
+month = age * 12
+day = age * 365
+hour = day * 24
+seconds = hour * 60 * 60
+
+print("your age in months is: " + str(month))
+print("your age in days is: " + str(day))
+print("your age in hours is: " + str(hour))
+print("your age in seconds is: " + str(seconds))

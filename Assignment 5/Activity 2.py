ba2f3cb1af7c0593209048798ef00bfc087fe91a
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-09-28 14:40:33 -0500

    Update Activity 2.py

--- Assignment 5/Activity 2.py
+++ Assignment 5/Activity 2.py
@@ -1,24 +1,47 @@
-  # This program will determan the users age in months days hours and seconds.
+# This program will determan the users age in months days hours and seconds.
 
-def Processinput (age) : 
-  pass
-def Processmonth (month) :
-  pass
-def Processday (day) :
-  pass
-def Processhour (hour) :
-  pass
-def Processseconds (seconds) :
-  pass
 
-age = int(input("How old are you? "))
+def display_results(month, day, hour, second):
+  print("your age in months is: " + str(month))
+  print("your age in days is: " + str(day))
+  print("your age in hours is: " + str(hour))
+  print("your age in seconds is: " + str(second))
 
-month = age * 12
-day = age * 365
-hour = day * 24
-seconds = hour * 60 * 60
 
-print("your age in months is: " + str(month))
-print("your age in days is: " + str(day))
-print("your age in hours is: " + str(hour))
-print("your age in seconds is: " + str(seconds))
+def get_age():
+  print("How old are you? ")
+  age = int(input())
+  return age
+
+
+def process_month(age):
+  month = age * 12
+  return month
+
+
+def process_day(age):
+  day = age * 365
+  return day
+
+
+def process_hour(day):
+  hour = day * 24
+  return hour
+
+
+def process_second(hour):
+  second = hour * 60 * 60
+  return second
+
+
+def main():
+  age = get_age()
+  month = process_month(age)
+  day = process_day(age)
+  hour = process_hour(day)
+  second =process_second(hour)
+  display_results(month, day, hour, second)
+  
+
+
+main() 

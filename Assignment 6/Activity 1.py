f5b06579dc71497ae6103f5f4fe99fc2d063879e
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-10-01 19:12:10 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,45 +1,45 @@
-  # This program displays the gross pay of an hourly worker.
+# This program displays the gross pay of an hourly worker.
 
 
 def display_results(weekly, monthly, annually):
-  print("your pay this Week is $" + str(weekly))
-  print("your pay this Month is $" + str(monthly))
-  print("your pay this Year is $" + str(annually))
+    print("your pay this Week is $" + str(weekly))
+    print("your pay this Month is $" + str(monthly))
+    print("your pay this Year is $" + str(annually))
 
 
 def get_hours():
-  print("Hours")
-  hours = int(input())
-  return hours
+    print("Hours")
+    hours = int(input())
+    return hours
 
 
 def get_rate():
-  print("rate")
-  rate = int(input())  
-  return rate
+    print("rate")
+    rate = int(input())
+    return rate
 
 
 def process_weekly(hours):
-  weekly = hours * rate
-  return weekly 
+    weekly = hours * rate
+    return weekly
 
 
 def process_monthly(weekly):
-  monthly = weekly * 4.33
-  return monthly
+    monthly = weekly * 4.33
+    return monthly
 
 
 def process_annually(monthly):
-  annually = monthly * 12
-  return annually   
+    annually = monthly * 12
+    return annually
 
 
 def main():
-  hours = get_hours() 
-  rate = get_rate()
-  weekly = process_weekly(hours)
-  monthly = process_monthly(weekly)
-  annually = process_annually(monthly)
+    hours = get_hours()
+    rate = get_rate()
+    weekly = process_weekly(hours)
+    monthly = process_monthly(weekly)
+    annually = process_annually(monthly)
 
 
 main()

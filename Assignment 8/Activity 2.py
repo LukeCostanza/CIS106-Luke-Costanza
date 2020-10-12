3fb93301cff4517d1b8a5c7a86fa6caf36e76071
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-10-11 22:10:18 -0500

    Create Activity 2.py

---
+++ Assignment 8/Activity 2.py
@@ -0,0 +1,25 @@
+# this application will determin the users average grade
+
+
+def getScores(grades):
+    print("Enter amount of " + grades + " you would like to enter:")
+    scores = int(input())
+
+    return scores
+
+
+def processTotal(scores):
+    while total == scores:
+        print(total)
+        total = total + scores
+
+    return total
+
+
+# Main
+def main()
+scores = getScores("grades")
+total = processTotal(scores, final)
+
+
+main()

9317f3558ae143af3038c79ba3bd61d905e31d3e
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-10-15 12:29:26 -0500

    Update Activity 1.py

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -1,46 +1,28 @@
-# This program will determin the final grade of the user
+# This prgram will return differnt multiples of the users input/parameters
 
 
-def get_stop():
-    print("Please combine all final grades and enter number her -->")
-    stop = int(input())
-    return stop
+def getValue(name):
+    print("Enter " + name + " value: ")
+    value = int(input())
+
+    return value
 
 
-def get_start():
-    print("Please enter the number zero") 
-    start = int(input()) 
-    return start
-
-
-def get_increment():
-    print("Enter your first grade:")
-    increment = int(input())
-    return increment 
-
-
-def get_increment2():
-    print("Enter your second grade:")
-    increment2 = int(input())
-    return increment2  
-
-
-def while_loop(start, stop, increment, increment2):
-    print("While loop counting from " + str(start) + " to " + 
-        str(stop) + " by " + str(increment) + " and " + str(increment2) + ":")
+def doLoop(start, stop, increment):
+    print("Do loop counting from " + str(start) + " to " + str(stop) + " by " + str(increment) + ".")
     count = start
-    while count <= stop:
+    while True:    #This simulates a Do Loop(I will be fixing this)
         print(count)
-        count += increment
-        count += increment2
+        count = count * increment
+        if not(count <= stop): break   #Exit loop
 
 
+# Main
 def main():
-    stop = get_stop()
-    start = get_start()
-    increment = get_increment()
-    increment2 = get_increment2()
-    while_loop(start, stop, increment, increment2)
+    start = getValue("starting")
+    stop = getValue("ending")
+    increment = getValue("increment")
+    doLoop(start, stop, increment)
 
 
-main()  
+main()

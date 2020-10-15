8340e8729de8af30f9dcc64972e83647b3b74a1f
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-10-15 12:20:52 -0500

    Update Activity 1.py

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -1,25 +1,46 @@
-# This program uses a loop to generate a list of multiplication expressions for a given value
+# This program will determin the final grade of the user
 
 
-def get_value(name):
-    print("Enter " + name + " value:")
-    value = int(input())
-    return value
+def get_stop():
+    print("Please combine all final grades and enter number her -->")
+    stop = int(input())
+    return stop
 
 
-def while_loop(number, expressions, multiple):
-    print (str("While looping your number " + str(number) + " will be multiplied by " + str(multiple) + " , " + str(expressions) + " times!"))
-    count = number
-    while count <= expressions:
+def get_start():
+    print("Please enter the number zero") 
+    start = int(input()) 
+    return start
+
+
+def get_increment():
+    print("Enter your first grade:")
+    increment = int(input())
+    return increment 
+
+
+def get_increment2():
+    print("Enter your second grade:")
+    increment2 = int(input())
+    return increment2  
+
+
+def while_loop(start, stop, increment, increment2):
+    print("While loop counting from " + str(start) + " to " + 
+        str(stop) + " by " + str(increment) + " and " + str(increment2) + ":")
+    count = start
+    while count <= stop:
         print(count)
-        count = count * multiple
+        count += increment
+        count += increment2
 
 
 def main():
-    number = get_value("number")
-    expressions = get_value("expressions")
-    multiple = get_value("multiple")
-    while_loop(number, expressions, multiple)
+    stop = get_stop()
+    start = get_start()
+    increment = get_increment()
+    increment2 = get_increment2()
+    while_loop(start, stop, increment, increment2)
 
 
-main()
+main()  

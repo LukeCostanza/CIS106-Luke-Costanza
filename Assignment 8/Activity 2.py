e0c52da7f61807236fdc29f4a926cb2943f6c1cb
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-10-17 17:16:38 -0500

    Update Activity 2.py

--- Assignment 8/Activity 2.py
+++ Assignment 8/Activity 2.py
@@ -1,46 +1,33 @@
-# This program will determin the final grade of the user
+# This program is determining the avergae grade for the user. 
 
 
-def get_stop():
-    print("Please combine all final grades and enter number her -->")
-    stop = int(input())
-    return stop
+def get_value():
+    print("How many scores would you like to enter?")
+    value = int(input())
+    return value
 
 
-def get_start():
-    print("Please enter the number zero") 
-    start = int(input()) 
-    return start
+count = 1
+sum = 0
 
 
-def get_increment():
-    print("Enter your first grade:")
-    increment = int(input())
-    return increment 
+def while_loop(count, value, score, sum):
+    while count <= value:
+      print("Enter score")
+      score = int(input())
+      sum = sum + score
+      count = count + 1
 
-
-def get_increment2():
-    print("Enter your second grade:")
-    increment2 = int(input())
-    return increment2  
-
-
-def while_loop(start, stop, increment, increment2):
-    print("While loop counting from " + str(start) + " to " + 
-        str(stop) + " by " + str(increment) + " and " + str(increment2) + ":")
-    count = start
-    while count <= stop:
-        print(count)
-        count += increment
-        count += increment2
+      
+def process_average(sum, value):      
+    average = sum / value
+    print("Your average grade is: " + str(average))
 
 
 def main():
-    stop = get_stop()
-    start = get_start()
-    increment = get_increment()
-    increment2 = get_increment2()
-    while_loop(start, stop, increment, increment2)
-
-
-main()  
+    value = get_value()
+    score = int(input())
+    while_loop(count, value, score, sum)
+    average = process_average(sum, value)
+ 
+main()

d043ee1b4f63523a2f90cf084bf031662a2d2e02
Dave Braunschweig <dave@itrain.ms>
2020-10-18 20:32:55 -0500

    Assignment 8 corrections

--- Assignment 8/Activity 2.py
+++ Assignment 8/Activity 2.py
@@ -1,4 +1,4 @@
-# This program is determining the avergae grade for the user. 
+# This program is determining the avergae grade for the user.
 
 
 def get_value():
@@ -11,24 +11,22 @@ def while_loop(value):
     count = 1
     sum = 0
     while count <= value:
-      print("Enter grade")
-      score = int(input())
-      sum = sum + score
-      count = count + 1
+        print("Enter grade")
+        score = int(input())
+        sum = sum + score
+        count = count + 1
     return sum
 
-      
-def process_average(sum, value):     
+
+def process_average(sum, value):
     average = sum / value
     print("Your average grade is: " + str(average))
-    
-    
-
 
 
 def main():
-  value = get_value()
-  sum = while_loop(value)
-  process_average(sum, value)
- 
+    value = get_value()
+    sum = while_loop(value)
+    process_average(sum, value)
+
+
 main()

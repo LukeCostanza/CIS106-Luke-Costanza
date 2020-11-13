53c3fec47af4aa7f9964e12a4e73cbf526a87e49
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-11-12 19:25:50 -0600

    Update Activity 1(1).py

--- Assignment 11/Activity 1(1).py
+++ Assignment 11/Activity 1(1).py
@@ -1,4 +1,6 @@
 #This program informs the user which year is a leap year. 
+#session 11 
+#change variable "r"
 
 
 def get_yinput():
@@ -13,23 +15,34 @@ def get_minput(yinput):
         print("Enter a month number you would like to look up: ")
         minput = int(input())
         if minput > 0 and minput < 13:
-            r = yinput % 4 
+            check = check_leapyear(yinput) 
             import array as arr
-            if r == 0:
+            if check == 0:
                 days = arr.array('i',[31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]) 
             else: 
                 days = arr.array('i',[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
-            months = ["janurary", "feburary", "march"]
+            months = ["Janurary", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
             print(months [minput - 1])
             print(days [minput - 1])
  
          
 def get_leap(yinput):
-    r = yinput % 4 
-    if r == 0:
+    check = check_leapyear(yinput)
+    if check == "Y":
         print ("It's a leap year!")
     else: 
         print("It's not a leap year!")
+
+
+def check_leapyear(year):
+    rem1 = year % 4
+    rem2 = year % 100
+    rem3 = year % 400
+    if ((rem1 == 0) and (rem2 != 0)) or (rem3 == 0):
+        answer = "Y"
+    else:
+        answer = "N"
+    return answer
 
 
 def main():
@@ -39,4 +52,4 @@ def main():
     print("Program end")
 
 
-main()    
+main()  

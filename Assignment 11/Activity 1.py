12239f3425d05d7537e8a5fdbf138e50043fccc1
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-11-12 18:12:15 -0600

    Create Activity 1.py

---
+++ Assignment 11/Activity 1.py
@@ -0,0 +1,53 @@
+#This program is determining the avergae grade for the user.
+
+
+def get_value():
+    print("How many grades would you like to enter?")
+    value = int(input())
+    return value
+
+
+def build_array(size):
+    array = []
+    for index in range(0, size):
+        print("Enter grade")
+        score = int(input())
+        array.append(score)
+    return array
+
+
+def get_high(size, arr):
+    high = 0
+    high = numpy.max(arr)
+    for index in range(0, size):
+        if arr[index] > high:
+            high = arr[index]
+    print("High score: " +str(high))
+
+
+def get_average(size, arr):
+    sum = 0
+    for index in range(0, size):
+        sum = sum + arr[index]
+    average = sum / size
+    print("Average score: " +str(average))
+
+
+
+def get_low(size, arr):
+    low = 999
+    for index in range(0, size):
+        if arr[index] < low:
+            low = arr[index]
+    print("Low score: " +str(low))     
+   
+
+def main():
+    value = get_value()
+    array = build_array(value)
+    get_high(value, array)
+    get_average(value, array)
+    get_low(value, array)
+
+
+main()

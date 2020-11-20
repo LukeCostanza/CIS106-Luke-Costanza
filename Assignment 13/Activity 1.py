75e583a8ecf4388335b37d09b815e83d52416676
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-11-19 20:01:21 -0600

    Update Activity 1.py

--- Assignment 13/Activity 1.py
+++ Assignment 13/Activity 1.py
@@ -3,35 +3,54 @@
 
 def get_name():
     while True:
-        print("Enter name (Last, First):")
-        name = input(str())
-        index = name.find(",")
-        if index >= 0:
-            break
-    return name
+        print("Enter name (First Last):")
+        name = input()
+        error_1 = get_error_1(name)
+        if error_1 == "n":
+            error_2 = get_error_2(name)
+            if error_2 == "n":
+                return name
+
+
+def get_error_1(name):
+    index = name.find(" ")
+    check = name[index + 1:]
+    index = check.find(" ")
+    if index >= 0:
+        print("Too many spaces!")
+        error_1 = "y"
+    else:
+        error_1 = "n"
+    return error_1
+
+
+def get_error_2(name):
+    last = get_last(name)
+    if last == "error":
+        print("No last name!")
+        error_2 = "y"
+    else:
+        error_2 = "n"
+    return error_2
 
 
 def get_last(name):
-    index = name.find("last, first")
-    if index < 0:
-        last = ""
+    index = name.find(" ")
+    if index > 0:
+        last = name[index + 1:]
     else:
-        last = name[0:index]
+        last = "error"
     return last
 
 
 def get_first(name):
-    index = name.find("last, first")
-    if index < 0:
-        first = ""
-    else:
-        first = name[index + 1:]
-        first = first.strip()
+    index = name.find(" ")
+    first = name[0: index]
     return first
 
 
 def display_name(first, last):
-    print(f"Hello {first} {last}!").format(first, last(1))
+    print(last + ", " + first[0] + ".")
 
 
 def main():

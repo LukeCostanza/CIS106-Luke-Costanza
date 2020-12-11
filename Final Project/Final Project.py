088cb83c574fdaf1756865b3071a53c6d8b509bb
LukeCostanza <70173487+LukeCostanza@users.noreply.github.com>
2020-12-10 20:32:20 -0600

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,14 +1,68 @@
-#This Program will print out the required data provided by a file and process given variables.
-    #https://stackabuse.com/reading-and-writing-xml-files-in-python/
-    
-    
-from xml.dom import minidom
+# This Program will print out the required data provided by a file and process given variables.
+# https://pymotw.com/3/xml.etree.ElementTree/parse.html
+
+from xml.etree import ElementTree
+
+
+#with open('plants.xml', 'rt') as f:
+tree = ElementTree.parse("plants.xml")
+
+
+def get_common():
+    common_arr = []
+    for x in tree.findall('PLANT'):
+        common = x.find('COMMON').text
+        common_arr.append(common)
+    return common_arr
+
+
+def get_botanical():
+    botanical_arr = []
+    for x in tree.findall('PLANT'):
+        botanical = x.find('BOTANICAL').text
+        botanical_arr.append(botanical)
+    return botanical_arr
+
+
+def get_zone():
+    zone_arr = []
+    for x in tree.findall('PLANT'):
+        zone = x.find('ZONE').text
+        zone_arr.append(zone)
+    return zone_arr
+
+
+def get_light():
+    light_arr = []
+    for x in tree.findall('PLANT'):
+        light = x.find('LIGHT').text
+        light_arr.append(light)
+    return light_arr
+
+
+def get_price():
+    price_arr = []
+    for x in tree.findall('PLANT'):
+        price = x.find('PRICE').text
+        price_arr.append(price)
+    return price_arr
+
+
+#for node in tree.iter():
+    #print(node.tag)
 
 
 def main():
-    mydoc = minidom.parse('plants.xml')
-    plants = mydoc.getElementsByTagName('COMMON')
-    print(plants[1].attributes['COMMON'].value)
+    common_arr = get_common()
+    botanical_arr = get_botanical()
+    zone_arr = get_zone()
+    light_arr = get_light()
+    price_arr = get_price()
+    print (common_arr[8])
+    print (botanical_arr[8])
+    print (zone_arr[8])
+    print (light_arr[8])
+    print (price_arr[8])
 
 
 main()
